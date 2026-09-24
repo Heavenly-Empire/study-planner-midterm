be3af629962c3d83668da815bf499c9ccca888f4
Necan <167892971+N3can@users.noreply.github.com>
2026-09-24 18:01:12 +0700

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Member 2: set `base` to `/<repository-name>/` before GitHub Pages deployment.
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  base: '/study-planner-midterm/',
+});
